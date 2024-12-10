a570791530c79d5ce7413296eaee05d36d5a4eb2
ikm216 <ikmclassof22@gmail.com>
2024-12-09 19:11:08 -0500

    test: added test to hash a given string given an enumerated hash algorithm

--- test.java
+++ test.java
@@ -1,5 +1,10 @@
 import org.junit.*;
 
 public class test{
-    
+    @Test
+    public void test_should_return_hash_string_enum(){
+        String string = "Chicken on Me";
+        String hash_string = hash.convertHashEnum(string, HashAlgorithm.SHA256);
+        Assert.assertNotNull(hash_string);
+    }
 }
