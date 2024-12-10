e73526db29452e4bccf6de67eaa1e61be7160326
ikm216 <ikmclassof22@gmail.com>
2024-12-09 20:34:56 -0500

    test: added test to generate a salted hash of a given string input, string salt, and enumerated hashing algorithm.

--- test.java
+++ test.java
@@ -13,4 +13,11 @@ public class test{
         String hash_string = hash.convertHashEnum(string, HashAlgorithm.SHA256);
         Assert.assertTrue(hash.matchHashString(string, hash_string, HashAlgorithm.SHA256));
     }
+    @Test
+    public void test_should_return_salted_hash() throws Exception{
+        String input = "Chicken on Me";
+        String salt = "Lemon Pepper";
+        String salt_hash = hash.saltHash(input, salt, HashAlgorithm.SHA256);
+        Assert.assertNotNull(salt_hash);
+    }
 }
