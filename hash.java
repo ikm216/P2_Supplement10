356f600604cd7d21cd25524aa63fa35e55cad03c
ikm216 <ikmclassof22@gmail.com>
2024-12-09 19:22:42 -0500

    test: revamp test to hash a given string given an enumerated hash algorithm

--- hash.java
+++ hash.java
@@ -1,3 +1,14 @@
-public class hash {
-    
-}
+// import java.security.*;
+// import java.util.Base64;
+
+// enum HashAlgorithm{
+//     MD5, SHA1, SHA256
+// }
+
+// public class hash {
+//     public static String convertHashEnum(String string, HashAlgorithm algorithm) throws NoSuchAlgorithmException{
+//         MessageDigest messagedigest = MessageDigest.getInstance(algorithm.name().replace("SHA", "SHA-"));
+//         byte[] bytes = messagedigest.digest(string.getBytes());
+//         return Base64.getEncoder().encodeToString(bytes);
+//     }
+// }
--- test.java
+++ test.java
@@ -2,7 +2,7 @@ import org.junit.*;
 
 public class test{
     @Test
-    public void test_should_return_hash_string_enum(){
+    public void test_should_return_hash_string_enum() throws Exception{
         String string = "Chicken on Me";
         String hash_string = hash.convertHashEnum(string, HashAlgorithm.SHA256);
         Assert.assertNotNull(hash_string);
